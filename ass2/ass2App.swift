547f73d3f5d065cf3c4b073a2d43f9adebe49fab
Abdul <127184231+101Viper777@users.noreply.github.com>
2024-04-02 17:59:26 +1100

    Initial Commit

---
+++ ass2/ass2App.swift
@@ -0,0 +1,17 @@
+//
+//  ass2App.swift
+//  ass2
+//
+//  Created by AbdulaIziz El sabbagh on 2/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct ass2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
